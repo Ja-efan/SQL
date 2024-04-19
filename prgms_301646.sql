c8daa4d35f7bbef347e2b4debcf251008b3a7b43
jaefan <woghks1213@gmail.com>
2024-04-19 15:21:30 +0900

    20240419 prgms 301646

---
+++ prgms_301646.sql
@@ -0,0 +1,18 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/301646
+# 특정 형질을 가지는 대장균 찾기 
+
+
+WITH A AS (
+    SELECT ID, GENOTYPE
+    FROM ECOLI_DATA
+    WHERE GENOTYPE & 2 = 0
+)
+SELECT COUNT(ID) AS COUNT
+FROM A
+WHERE GENOTYPE & POWER(2, (1-1)) = POWER(2, (1-1)) 
+    OR GENOTYPE & POWER(2, (3-1)) = POWER(2, (3-1));
+
+
+SELECT COUNT(ID) AS COUNT
+FROM ECOLI_DATA
+WHERE GENOTYPE & 2 = 0 AND (GENOTYPE & 4 = 4 OR GENOTYPE & 1 = 1);
